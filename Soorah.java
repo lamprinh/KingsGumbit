604540be960d750ffd0ccd1c8699bb6d84d9ef94
lamprinh <t8200055@aueb.gr>
2021-11-13 21:23:35 +0200

    add Soorah.java

---
+++ Soorah.java
@@ -0,0 +1,19 @@
+
+public class Soorah {
+
+    private static int frame;
+
+    public static int getAirframe() {
+
+        return frame = 660;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Soorah.getAirframe());
+        System.out.println();
+        System.exit(0);
+    }
+
+}
+
