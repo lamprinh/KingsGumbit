a1f267362a95802b8850c9fdc230e8d557cf2758
Plateia viktorias <94313627+Stratisgi@users.noreply.github.com>
2021-11-17 14:53:47 +0200

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,1 +1,62 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Game {
+	Scanner input = new Scanner(System.in);
+	public static  String[] Knowledge_cards;
+	public static  String[] Menu_cards;
+	boolean[] kn_cards;
+	int players;
+	Random dicenumber=new Random();
+	public Game(int players) {
+		this.players=players;
+	}
+	public boolean Seat_check(int seat) {
+		if (kn_cards[seat]=true) {
+			return true;
+
+		}
+		else {
+			return false ;
+		}
+	}
+	public boolean buy() {
+		System.out.println("Would you like to buy this Area?(Yes/No)");
+		String answer = input.nextLine();
+		if (answer=="YES") {
+			return true; }
+		else {
+			return false; }
+
+
+		}
+			public int roll_1() {
+		 int dice;
+		 dice = 1 +dicenumber.nextInt(6);
+		 return dice;
+		}
+		public int roll_2(int pos) {
+			int x=0;
+			if (pos>1 && pos<6) {
+				x= 1+dicenumber.nextInt(10);
+			}else if (pos>6 && pos<12) {
+				x= 11 +dicenumber.nextInt(20);
+			}else if (pos>12 && pos<18) {
+				x= 21 +dicenumber.nextInt(30);
+			}else if (pos>18 && pos<24){
+				x= 31 +dicenumber.nextInt(40);
+			}
+			return x;
+		}
+		public int roll_3() {
+		 int x;
+		 x= 1 +dicenumber.nextInt(15);
+		 return x;
+}
+	public int move(int pos) {
+	  pos = pos + roll_1();
+	  return pos;
+	}
+	}
+
+
 
