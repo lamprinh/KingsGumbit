e09a7e06e9bd8d217cdd2140a043bb5249a70545
Stratis Giannakas <94313627+Stratisgi@users.noreply.github.com>
2021-11-23 22:35:38 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -4,13 +4,13 @@ public class Game {
 	Scanner input = new Scanner(System.in);
 	public static  String[] Knowledge_cards; // πινακας με τις καρτες ερωτήσεων για τις περιοχες
 	public static  String[] Menu_cards; // πινακας για καρτες εντολων( προχωρα 2 θεσεις κτλ)
-	boolean[] kn_cards; // πινακας που δείχνει αν η περιοχη ανήκει η οχι σε καποιον
+	String[] kn_cards; // πινακας που δείχνει αν η περιοχη ανήκει η οχι σε καποιον
 	int players;
 	Random dicenumber=new Random();
 	public Game(int players) { // κατασκευαστης θετει τον αριθμο των παικτων ανάλογα με την είσοδο που θα δωσουν οι χρηστες
 		this.players=players;
 	}
-	public boolean Seat_check(int seat) { // ελεγχει αν ανηκει η περιοχη σε καποιον παικτη
+	public static boolean Seat_check(int seat) { // ελεγχει αν ανηκει η περιοχη σε καποιον παικτη
 		if (kn_cards[seat]=true) {
 			return true;
 
@@ -19,7 +19,7 @@ public class Game {
 			return false ;
 		}
 	}
-	public boolean buy() { //μεθοδος που δινει την επιλογη αγορας 
+	public static boolean buy() { //μεθοδος που δινει την επιλογη αγορας 
 		System.out.println("Would you like to buy this Area?(Yes/No)");
 		String answer = input.nextLine();
 		if (answer=="YES") {
@@ -29,12 +29,12 @@ public class Game {
 
 
 		}
-			public int roll_1() { //μεθοδος που φερνει αποτελεσμα στο ζαρι (1-6)
+			public static int roll_1() { //μεθοδος που φερνει αποτελεσμα στο ζαρι (1-6)
 		 int dice;
 		 dice = 1 +dicenumber.nextInt(6);
 		 return dice;
 		}
-		public int roll_2(int pos) { // μεθοδος που τραβαει τυχαια καρτα ερωτησης για τις περιοχες αναλογα με το που βρισκεται ο παικτης 
+		public static  int roll_2(int pos) { // μεθοδος που τραβαει τυχαια καρτα ερωτησης για τις περιοχες αναλογα με το που βρισκεται ο παικτης 
 			int x=0;
 			if (pos>1 && pos<6) {
 				x= 1+dicenumber.nextInt(10);
@@ -47,15 +47,16 @@ public class Game {
 			}
 			return x;
 		}
-		public int roll_3() { //μεθοδος που τραβαει τυχαια καρτα απ το μενου εντολων
+		public static int roll_3() { //μεθοδος που τραβαει τυχαια καρτα απ το μενου εντολων
 		 int x;
 		 x= 1 +dicenumber.nextInt(15);
 		 return x;
 }
-	public int move(int pos) { //μεθοδος που μετακινει τον χαρακτηρα με βαση το ζαρι που εφερε
+	public static int move(int pos) { //μεθοδος που μετακινει τον χαρακτηρα με βαση το ζαρι που εφερε
 	  pos = pos + roll_1();
 	  return pos;
 	}
+	
 	}
 
 
