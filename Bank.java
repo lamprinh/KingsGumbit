eb9ba06444a022ff27bd730c13a2d909b803d259
lamprinh <93085770+lamprinh@users.noreply.github.com>
2021-12-07 00:26:19 +0200

    Update Bank.java
    Μετατροπη κλασης bank και μεθοδων αυτης σε abstract

--- Bank.java
+++ Bank.java
@@ -1,88 +1,71 @@
-
-import java.util.Scanner;
-
-public class Bank {
-	static Scanner input = new Scanner(System.in);
-	private int money;
+public abstract class Bank {
+	protected int money;
 	public Bank() {// κατασκευαστής για το πέρασμα της τιμής στην μεταβλητή money
-		this.money = 300000;
-	}
-	
-	public int getMoney() {
-		return this.money;
-	}
-	
-	public void setMoney(int money) {
-		this.money = money;
+		money = 300000;
 	}
 	
 	public void shareMoney(King k) { // μέθοδος που θα καλείται στην αρχή του παιχνιδιού για το μοιρασμα ενός χρηματικού ποσού 
 		                             //σε καθέναν από τους παίκτες
 		System.out.println("The game starts , the kings will receive a sum of money.");
 		k.wallet += 2000;
-		this.money -= 2000;
+		money -= 2000;
 	}
 	
-	static Bank pay = new Bank();
-	
-	
-	public static void giveMoney(King k, String kn_cards[] ) { //  μέθοδος που καλείται οταν δώσει ο εκάστοτε παίκτης την απάντηση του σε ερώτηση που θα του τύχει 
-		                                                       // με σκόπο να πάρει ή να δώσει στον παίκτη ενα χρηματικό ποσό 
+	public abstract int giveMoney(King k, String []Knowledge_cards ); //{ // μέθοδος που καλείται οταν δώσει ο εκάστοτε παίκτης την απάντηση του σε ερώτηση που θα του τύχει 
+		                                                       //με σκόπο να πάρει ή να δώσει στον παίκτη ενα χρηματικό ποσό 
 		                                                       //έπειτα ακολουθεί η διαδικασία της αγοάς της περιοχής σε περίπτωση που η απάντηση είναι σωστή 
-	    boolean belong;
-		boolean ans;
-		boolean flag = false;
-		int pos = 0;
-		int position;
-		position = Game.move(pos);
-		boolean answ = true;
-		answ = Game.anwsercheck(pos);
-		if(answ == true) {
-			System.out.println("The answear was right, the king will receive 1500$");
-			k.wallet += 1500;
-			if(flag==false) {
-				System.out.println("Area does not exist");
-			} else {
-				ans = Game.buy();
-			    if (ans == true) {
-			    	System.out.println("The king wants to buy the area.");
-			    	belong = Game.Seat_check(position);
-			    	if(belong == false) {
-			    		  Bank.buyProperty(k, null, kn_cards);
-			    	} else {
-			    		  System.out.println("Property belongs to "+ kn_cards[position]+"you have to pay a tax.");
-			    		  Bank.transfer(k, k, null);
-			    		}
-			    } else {
-			    	System.out.println("The king does not want to buy this area.");
-			    }
-		    }
-	    } else {
-	    	System.out.println("The question is wrong, you have to pay.");
-	    	pay.takeMoney(k);
-	    }
-	}
+	  
 	
-	public void takeMoney(King k) {//μέθοδος που παίρνει χρηματικό ποσό από παίκτη κ το μεταφέρει στην τράπεζα αν η απάντηση είναι λάθος .
-			k.wallet -= 1200;
-			this.money += 1200;
-    }
+	public abstract int takeMoney(King k); //{// μέθοδος που παίρνει χρηματικό ποσό από παίκτη κ το μεταφέρει στην τράπεζα αν η απάντηση είναι λάθος.
      
-    public static String[] Prop = new String[3];// δημιουργλια 2 μονοδιάστατων πίνακων παρράλληλων που αντιστοιχούν σε
+    public static String[] Prop = new String[20];// δημιουργλια 2 μονοδιάστατων πίνακων παρράλληλων που αντιστοιχούν σε 
                                                 //	περιοχές και τιμές
-	                                            // πρέπει να καλεστούν από τη main για να αρχικοποιηθούν
+	                                            //  πρέπει να καλεστούν από τη main για να αρχικοποιηθούν
 	public static void setNamesArray() {
 		Prop[0]= "Athina";
 		Prop[1]= "Sparti";
 		Prop[2]= "Lamia";
+		Prop[3]= "Lamia";
+		Prop[4]= "Lamia";
+		Prop[5]= "Lamia";
+		Prop[6]= "Lamia";
+		Prop[7]= "Lamia";
+		Prop[8]= "Lamia";
+		Prop[9]= "Lamia";
+		Prop[10]= "Lamia";
+		Prop[12]= "Lamia";
+		Prop[13]= "Lamia";
+		Prop[14]= "Lamia";
+		Prop[15]= "Lamia";
+		Prop[16]= "Lamia";
+		Prop[17]= "Lamia";
+		Prop[18]= "Lamia";
+		Prop[19]= "Lamia";		
 	}
 	
-	public static int[] Price = new int[3];
+	public static int[] Price = new int[20];
 	public static void setPricesArray() {
 		Price[0]= 80000;
 		Price[1]= 120000;
 		Price[2]= 90000;
-		}	
+		Price[3]= 90000;
+		Price[4]= 90000;
+		Price[5]= 90000;
+		Price[6]= 90000;
+		Price[7]= 90000;
+		Price[8]= 90000;
+		Price[9]= 90000;
+		Price[10]= 90000;
+		Price[11]= 90000;
+		Price[12]= 90000;
+		Price[13]= 90000;
+		Price[14]= 90000;
+		Price[15]= 90000;
+		Price[16]= 90000;
+		Price[17]= 90000;
+		Price[18]= 90000;
+		Price[19]= 90000;
+	}	
 	
 	public static int[] getPrices(int[] t) {// δημιουργία 3 μεθόδων get για να μπορούν οι άλλες κλάσεις καλούν τους πίνακες
 		t = Price;
@@ -102,7 +85,7 @@ public class Bank {
 		k.wallet += 10000;
 	}
 	
-	public static void buyProperty(King k, String p, String kn_cards[]) {//  μέθοδος που θα καλείται για να αγοράσει ένας βασιλιάς μια περιοχή από την τράπεζα
+	public static void buyProperty(King k, String p, String kn_cards[]) {// μέθοδος που θα καλείται για να αγοράσει ένας βασιλιάς μια περιοχή από την τράπεζα
 		
 		boolean flag = false;
 		
@@ -135,21 +118,23 @@ public class Bank {
 		}
 	}
     
-	public static void transfer(King k1, King k2, String p) {//  μέθοδος που θα καλείται όταν ένας βασιλιάς θα πρέπει να πληρώσει φόρο σε εναν άλλο 
+//public static void transfer(King k1, King k2, String p) {// μέθοδος που θα καλείται όταν ένας βασιλιάς θα πρέπει να πληρώσει φόρο σε εναν άλλο 
 		
-		int y=0;
-		int point=99;
-		boolean flag = false;
-		while (y <= Prop.length && !flag) {
-			if(p == Prop[y]) {
-				point=y;
-				flag=true;
-			}
-			y++;
-		}
-		if(point !=99) {
-		k1.wallet -= tax(Price[point]);
-		k2.wallet += tax(Price[point]);
-		}
-	}	
+		//int y=0;
+		//int point=99;
+		//boolean flag = false;
+		//while (y <= Prop.length && !flag) {
+			//if(p == Prop[y]) {
+				//point=y;
+				//flag=true;
+			//}
+			//y++;
+		//}
+		//if(point !=99) {
+		//k1.wallet -= tax(Price[point]);
+		//k2.wallet += tax(Price[point]);
+		//}
+	//}	 
+	
 }
+
