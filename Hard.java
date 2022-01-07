7bbbe3a31e8b9e2d7ecf73eaec7c8ef5d6b2adb1
lamprinh <93085770+lamprinh@users.noreply.github.com>
2022-01-07 18:04:00 +0200

    Add files via upload

---
+++ Hard.java
@@ -0,0 +1,286 @@
+import java.util.ArrayList;
+
+public class Hard extends Bank {
+	static ArrayList<String> hard_q1 = new ArrayList<String>() { {
+		hard_q1.add("Ποια ήταν η θεά της δικαιοσύνης; ");
+		hard_q1.add("Οι γονείς του Ηρακλή ήταν ο Δίας και η ");
+		hard_q1.add("Ο ουρανός γεννήθηκε από την ");
+		hard_q1.add("Ποιος θεός έχει ως ιερό ζώο τα άλογα; ");
+		hard_q1.add("Πώς λεγόταν η γυναίκα του Ποσειδώνα; ");
+		hard_q1.add("Με ποιόν έπιασε στο κρεβάτι την Αφροδίτη ο Ήφαιστος; ");
+		hard_q1.add("Ο Θησέας στην πραγματικότητα ήταν γιός της Αίθρας και του ");
+		hard_q1.add("Ποια ήταν η κόρη της Κλυταιμνήστρας;");
+		hard_q1.add("Ο Οιδίποδας ήταν βασιλιάς της ");
+		hard_q1.add("Πώς έλεγαν τον πατέρα της ωραίας Ελένης; ");
+		hard_q1.add("Βασιλιάς της Θράκης γνωστός κυρίως για τα άλογα που του έκλεψε ο Οδυσσέας ήταν ο ");
+		hard_q1.add("Ποιόν έστειλε στη γη ο παντοδύναμος Δίας για να εκδικηθεί τον Προμηθέα; ");
+		hard_q1.add("Ποιος ήταν ο μυθικός λυράρης που έσωσε τους Αργοναύτες από τις Σειρήνες; ");
+		hard_q1.add("Ποιος ήταν ο πατέρας των παιδιών της Μήδειας; ");
+		hard_q1.add("Η Ερυάλη, η Μέδουσα και η Σθενώ, σύμφωνα με τη μυθολογία ήταν ");
+		hard_q1.add("Ποια ήταν η πρώτη σύζυγος του Ηρακλή; ");
+		hard_q1.add("Ποιος ήταν ο πατέρας της Περσεφόνης; ");
+		hard_q1.add("Ποιος ήταν ο πατέρας του Οδυσσέα;");
+		hard_q1.add("Ποια ήταν η θεότητα της νεότητας και της ζωντάνιας; ");
+		hard_q1.add("Ποιος ήταν ο θεός της χλεύης, της ειρωνείας και του σαρκασμού; ");
+	} };
+	
+	static ArrayList<String> hard_q2 = new ArrayList<String>() { {
+		hard_q2.add("Που βρισκόταν ο θησαυρός των Σιφνίων;  ");
+		hard_q2.add("Που γεννήθηκε ο Πλούταρχος;  ");
+		hard_q2.add("Ποιος έγραψε το έργο «Η πολιτεία»; ");
+		hard_q2.add("Στο ολιγαρχικό καθεστώς κριτήριο διάκρισης πολιτών ήταν ");
+		hard_q2.add("Ποιο πολίτευμα ονομαζόταν και «τιμοκρατικό»; ");
+		hard_q2.add("Ποιος πολιτικός ρήτορας ήταν αντίθετος της Πανελλήνιας ιδέα; ");
+		hard_q2.add("Ποιος ήταν ο τελευταίος βασιλιάς της Αθήνας; ");
+		hard_q2.add("Με ποια μάχη επιβλήθηκαν στρατιωτικά στις πόλεις της νότιας Ελλάδας οι Μακεδόνες; ");
+		hard_q2.add("Σε ποια πόλη ήταν νομοθέτης ο Λυκούργος; ");
+		hard_q2.add("Πως λεγόταν στη δημοκρατία η δυνατότητα κάθε πολίτη να παίρνει το λόγο και να λέει ελεύθερα την άποψή του;  ");
+		hard_q2.add("Ο Περίανδρος ήταν τύραννος στην ");
+		hard_q2.add("Ποια περιοχή είχε τους πιο φημισμένους καβαλάρηδες; ");
+		hard_q2.add("Ποιος πρωτότυπος Έλληνας στρατηγός χρησιμοποίησε τους Πολεμικούς Ελέφαντες; ");
+		hard_q2.add("Τους πιο μάχιμους Σπαρτιάτες που αποτελούσαν επίλεκτη δύναμη τους αποκαλούσαν  ");
+		hard_q2.add("Από ποια ηλικία ξεκινούσε η στρατιωτική εκπαίδευση των νεαρών Σπαρτιατών; ");
+		hard_q2.add("Από ποια περιοχή κατάγονται οι είλωτες; ");
+		hard_q2.add("Πόσα ήταν τα ιερά πλοία της αρχαίας Αθήνας;  ");
+		hard_q2.add("Εναντίον ποιας ελληνικής αποικίας στη Σικελία εκστράτευσαν οι Αθηναίοι το 415π.Χ. ;");
+		hard_q2.add("Πως ονομάζονταν τα αρχαία τείχη της Αθήνας που την ένωναν με τον Πειραιάς ");
+		hard_q2.add("Σε ποιο λόφο της Αθήνας λάμβανε χώρα η Εκκλησία του Δήμου; ");
+	} };
+	
+	static ArrayList<String> hard_q3 = new ArrayList<String>() { {
+		hard_q3.add("Ο Παπαφλέσσας ασκούσε καθήκοντα υπουργού...  ");
+		hard_q3.add("Στη μάχη στο Μανιάκι νίκησε ο... ");
+		hard_q3.add("Ο Δ. Πλαπούτας γεννήθηκε το 1786 στην ...");
+		hard_q3.add("Τη περίοδο της αντιβασιλείας ο Πλαπούτας καταδιώχθηκε μαζί με τον Κολοκοτρώνη ως... ");
+		hard_q3.add("Πλαπούτας και Κολοκοτρώνης καταδικάστηκαν σε...το 1834.");
+		hard_q3.add("Ο Α. Διάκος εντάχθηκε στο σώμα του ...σε ηλικία 17 ετών.");
+		hard_q3.add("Η Μπουμπουλίνα είχε καταγωγή από την ...");
+		hard_q3.add("Η Μπουμπουλίνα πέθανε στις 22/5/1825 στις...");
+		hard_q3.add("Το πλοίο της Μπουμπουλίνα που πήρε μέρος στην ελληνική επανάσταση ονομαζόταν...");
+		hard_q3.add("Γυναίκα μέλος φιλικής εταιρείας.");
+		hard_q3.add("Ο Ι. Καποδίστριας δολοφονήθηκε στο...");
+		hard_q3.add("\"γραμματικός\" του Οδ. Ανδρούτσου ήταν ο Αντώνιος... ");
+		hard_q3.add("Κόρη εμπόρου και μέλους φιλικής εταιρείας.");
+		hard_q3.add("Διέθεσε όλη της την περιουσία για τον αγώνα.");
+		hard_q3.add("Η σφαγή της Χίου έγινε 11  ...   1822.");
+		hard_q3.add("Περίπου 42000 χριστιανοί σφαγιάστηκαν στη... ");
+		hard_q3.add("Στις 6/7/118822 έχουμε την ανατίναξη της τουρκικής ναυαρχίδας από τον Κωσταντίνο... ");
+		hard_q3.add("Τα ...παρέμειναν στην οθωμανική αυτοκρατορίας έως το 1912.");
+		hard_q3.add("Ο Γεώργιος ...εντάχθηκε σε κλέφτικη ομάδα από τα 15 του.");
+		hard_q3.add("Ο Α εμφύλιος πόλεμος ξεκίνησε το ...του 1823.");
+	} };
+	
+	static ArrayList<String> hard_q4 = new ArrayList<String>() { {
+		hard_q4.add("Το καθεστώς του Μουσολίνι θεωρούνταν ... ");
+		hard_q4.add("Ο Μεταξάς διορίστηκε πρωθυπουργός της Ελλάδας από τον Βασιλιά.");
+		hard_q4.add("Ο Μεταξάς έμεινε στην ιστορία για την αρνητική απάντηση που έδωσε στο ...");
+		hard_q4.add("Το 1994 αναγνωρίστηκε από τη Βουλή των Ελλήνων η ... των Ποντίων.");
+		hard_q4.add("Ο Κεμάλ Ατατουρκ ήταν ...του τουρκικού στρατού.");
+		hard_q4.add("Ο Κεμάλ σε μυστική συμφωνία με τους Γάλλους ακύρωσε τη συμφωνία των ...");
+		hard_q4.add("Η συμφωνία που συνομολογήθηκε μεταξύ Γαλλίας και κυβέρνησης ’γκυρας ονομάστηκε συνθήκη της ...");
+		hard_q4.add("Η συνθήκη της Λοζάνης υπογράφτηκε στην ...");
+		hard_q4.add("Το 1923 συμφωνήθηκε μεταξύ Ελλάδας και Τουρκίας η ανταλλαγή ...");
+		hard_q4.add("Μεταξύ των ανταλλάξιμων πληθυσμών περιλαμβάνονταν και Έλληνες του ...");
+		hard_q4.add("Η ρωσική επανάσταση είχε μια σειρά από ιστορικά γεγονότα που επηρέασαν την ...");
+		hard_q4.add("Η επανάσταση του ...σηματοδότησε την απαρχή της ρωσικής επανάστασης.");
+		hard_q4.add("Η οκτωβριανή επανάσταση είναι γνωστή και ως ... Οκτώβριος.");
+		hard_q4.add("Η συνθήκη των ...τερμάτισε τον Α παγκόσμιο πόλεμο.");
+		hard_q4.add("Η Ιταλία υπήρξε μία από τις...");
+		hard_q4.add("Η μεγάλη ιδέα σχετίζονταν με την απελευθέρωση των περιοχών που ζούσαν οι ελληνικοί πληθυσμοί στην ...");
+		hard_q4.add("Ο Αδόλφος Χίτλερ είχε την προσωνυμία ...");
+		hard_q4.add("Ο Χίτλερ ήταν Γερμανός ...της Ναζιστικής Γερμανίας.");
+		hard_q4.add("Ο Χίτλερ διετέλεσε ...της Γερμανίας.");
+		hard_q4.add("Ο Χίτλερ υπέφερε από την νόσο ...");
+	} };
+	
+	static ArrayList<String> hard_correct1 = new ArrayList<String>() { {
+		hard_correct1.add("Θέμις");
+		hard_correct1.add("Αλκμήνη");
+		hard_correct1.add("Γαία");
+		hard_correct1.add("Ποσειδώνας");
+		hard_correct1.add("Αμφιτρίτη");
+		hard_correct1.add("’ρη");
+		hard_correct1.add("Ποσειδώνας");
+		hard_correct1.add("Ιφιγένεια)");
+		hard_correct1.add("Θήβας");
+		hard_correct1.add("Τυνδάρεω");
+		hard_correct1.add("Ρήσος");
+		hard_correct1.add("Την Πανδώρα");
+		hard_correct1.add("Ορφέας");
+		hard_correct1.add("Ιάσονας");
+		hard_correct1.add("Γοργόνες");
+		hard_correct1.add("Μεγάρα");
+		hard_correct1.add("Δίας");
+		hard_correct1.add("Λαέρτης");
+		hard_correct1.add("Ήβη)");
+		hard_correct1.add("Μώμος");
+	} };
+
+	static ArrayList<String> hard_correct2 = new ArrayList<String>() { {
+		hard_correct2.add("Στους Δελφούς");
+		hard_correct2.add("Βοιωτία");
+		hard_correct2.add("Πλάτωνας");
+		hard_correct2.add("Εισόδημα");
+		hard_correct2.add("Η Ολιγαρχία");
+		hard_correct2.add("Ο Δημοσθένης");
+		hard_correct2.add("Ο Κόρδος");
+		hard_correct2.add("Της Χαιρώνειας");
+		hard_correct2.add("Στην Σπάρτη");
+		hard_correct2.add("Ισηγορία");
+		hard_correct2.add("Κόρινθο");
+		hard_correct2.add("Η Θεσσαλία");
+		hard_correct1.add("Ο Πύρρος");
+		hard_correct1.add("Σκιρίτες");
+		hard_correct1.add("Επτά");
+		hard_correct1.add("Μεσσηνία");
+		hard_correct2.add("Έξι");
+		hard_correct2.add("Συρακούσες)");
+		hard_correct2.add("Μακρά τείχη");
+		hard_correct2.add("Πνύκα");
+	} };
+	
+	static ArrayList<String> hard_correct3 = new ArrayList<String>() { {
+		hard_correct3.add("Στρατιωτικών");
+		hard_correct3.add("Ιμπραήμ");
+		hard_correct3.add("Αρκαδία");
+		hard_correct3.add("Ρωσόφιλος");
+		hard_correct3.add("Θάνατο");
+		hard_correct3.add("Σκαλτσά");
+		hard_correct3.add("Ύδρα");
+		hard_correct3.add("Σπέτσες");
+		hard_correct3.add("Αγαμέμνων");
+		hard_correct3.add("Μπουμπουλίνα");
+		hard_correct3.add("Ναύπλιο");
+		hard_correct3.add("Γεωργαντάς");
+		hard_correct3.add("Μαυρογένους");
+		hard_correct3.add("Μαυρογένους");
+		hard_correct3.add("Μαρτίου");
+		hard_correct3.add("Χίο");
+		hard_correct3.add("Κανάρη");
+		hard_correct3.add("Ψαρά");
+		hard_correct3.add("Καραϊσκάκης");
+		hard_correct3.add("Φθινόπωρο");
+	} };
+	
+	static ArrayList<String> hard_correct4 = new ArrayList<String>() { {
+		hard_correct4.add("Φασιστικό");
+		hard_correct4.add("Γεώργιο");
+		hard_correct4.add("Ιταλικό");
+		hard_correct4.add("Γενοκτονία");
+		hard_correct4.add("Αρχιστράτηγος");
+		hard_correct4.add("Σεβρών");
+		hard_correct4.add("’γκυρας");
+		hard_correct4.add("Ελβετία");
+		hard_correct4.add("Πληθυσμών");
+		hard_correct4.add("Πόντου");
+		hard_correct4.add("Παγκόσμια ιστορία");
+		hard_correct4.add("Φεβρουαρίου");
+		hard_correct4.add("Κόκκινος");
+		hard_correct4.add("Βερσαλλιών");
+		hard_correct4.add("Μεγάλες δυνάμεις");
+		hard_correct4.add("Οθωμανική αυτοκρατορία");
+		hard_correct4.add("Φύρερ");
+		hard_correct4.add("Δικτάτορας");
+		hard_correct4.add("Καγκελάριος");
+		hard_correct4.add("Πάρκινσον");
+	} };
+	
+	public static void Showquestion( int i, Player p) {
+		  if (i == 1) {
+	          if (p.getPos() <=6) {
+		          System.out.println("Question is:" + hard_q1.get(p.getPos()));
+	          } else if (p.getPos() <=11) {
+		          System.out.println("Question is:" + hard_q2.get(p.getPos()));
+	          } else if (p.getPos() <=16) {
+		          System.out.println("Question is:" + hard_q3.get(p.getPos()));
+	          } else {
+		          System.out.println("Question is:" + hard_q4.get(p.getPos()));
+		      }
+	      } else if (i == 2) {
+		      if (p.getPos() <=6) {
+			      System.out.println("Question is:" + hard_q1.get(p.getPos()+5));
+		      } else if (p.getPos() <=11) {
+			      System.out.println("Question is:" + hard_q2.get(p.getPos()));
+		      } else if (p.getPos() <=16) {
+			      System.out.println("Question is:" + hard_q3.get(p.getPos()-5));
+		      } else {
+			      System.out.println("Question is:" + hard_q4.get(p.getPos()-10));
+		      }
+	      } else if (i == 3) {
+		      if (p.getPos() <=6) {
+			      System.out.println("Question is:" + hard_q1.get(p.getPos()+10));
+		      } else if (p.getPos() <=11) {
+			      System.out.println("Question is:" + hard_q2.get(p.getPos()+5));
+		      } else if (p.getPos() <=16) {
+			      System.out.println("Question is:" + hard_q3.get(p.getPos()));
+		      } else {
+			      System.out.println("Question is:" + hard_q4.get(p.getPos()-5));
+		      }
+	      } else {		  
+	          if (p.getPos() <=6) {
+		          System.out.println("Question is:" + hard_q1.get(p.getPos()+15));
+	          } else if (p.getPos() <=11) {
+		          System.out.println("Question is:" + hard_q2.get(p.getPos()+10));
+	          } else if (p.getPos() <=16) {
+		          System.out.println("Question is:" + hard_q3.get(p.getPos()+5));
+	          } else {
+		          System.out.println("Question is:" + hard_q4.get(p.getPos()));
+		      }
+	     }
+	  }
+	
+	@Override
+	public void giveMoney(int i, Player p) {
+		if(Hard_answer.Check_Answer(p, i, null) == true) {
+			if (Game.Seat_check(p.getPos()) == false) {
+				int x = p.wallet + Bank.getAmount() + 2000;
+				p.setWallet(x);
+				Bank.setMoney(x);
+			}
+		}
+	}
+
+	@Override
+	public void takeMoney(int i, Player p) {
+		if(Hard_answer.Check_Answer(p, i, Hard_answer.show_answer(p)) == false) {
+			if(Game.Seat_check(p.getPos()) == false) {
+				int x = p.wallet + Bank.getAmount() - 800;
+				p.setWallet(x);
+				Bank.setMoney(x);
+			}
+		}
+	}
+
+	@Override
+	public void transfer(int i, Player p, Player p0) {
+		if(Game.Seat_check(p.getPos()) == true) {
+			if(Hard_answer.Check_Answer(p, i, Hard_answer.show_answer(p)) == true) {
+				int x = p.wallet - Bank.getAmount();
+				p.setWallet(x);
+				int z = p0.wallet + Bank.getAmount();
+				p0.setWallet(z);
+			}else {
+				int x = p.wallet - (int)(Bank.getAmount() - Bank.getAmount()*0.3);
+				p.setWallet(x);
+				int z = p0.wallet + (int)(Bank.getAmount() - Bank.getAmount()*0.3);
+				p0.setWallet(z);
+		    } 
+		}
+	}
+	
+	@Override
+	public void buyProperty(int i, Player p, int [] King_area) {
+		if(Game.Seat_check(p.getPos()) == true) {
+			if(Game.buy() == true) {
+				if(Hard_answer.Check_Answer(p, i, Hard_answer.show_answer(p))== true) {
+					int x = p.wallet - (int) (Price[p.getPos()] - Price[p.getPos()] * 0.3);
+				    p.setWallet(x);
+					King_area[p.getPos()] = i;
+				}
+			} else {
+				System.out.println("The player decided not to buy the area.");
+			}
+		}
+	}
+}
+	
