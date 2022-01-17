b5478b011cbe68224e0b5b6a693dbff71e5b9af2
lamprinh <93085770+lamprinh@users.noreply.github.com>
2022-01-17 16:50:19 +0200

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,47 @@
+
+public class Player {
+	public int wallet;
+	public int pos;
+	public int round;
+	public String character = null;
+
+	public Player() {
+		wallet = 0;
+		pos = 0;
+		round = 1;
+		character = null;
+	}
+
+	public int getWallet() {
+		return wallet;
+	}
+
+	public void setWallet(int wallet) {
+		this.wallet += wallet;
+	}
+
+	public int getPos() {
+		return pos;
+	}
+
+	public void setPos(int pos) {
+		this.pos += pos;
+	}
+
+	public int getRound() {
+		return round;
+	}
+
+	public void setRound(int round) {
+		this.round = round;
+	}
+
+	public String getCharacter() {
+		return character;
+	}
+
+	public void setCharacter(String character) {
+		this.character = character;
+	}
+
+}
