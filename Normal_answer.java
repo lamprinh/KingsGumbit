82ac54c188e151b61bf4252722580c2b00130365
lamprinh <93085770+lamprinh@users.noreply.github.com>
2022-01-18 18:21:26 +0200

    Add files via upload

---
+++ Normal_answer.java
@@ -0,0 +1,166 @@
+import java.util.Scanner;
+
+public class Normal_answer {
+	static Scanner input = new Scanner(System.in);
+
+	public static boolean show_answer(int i, Player[] arr) {
+		boolean answ = false;
+		if (i == 0) {
+			if (arr[i].getPos() <= 5) {
+				System.out.println("The answers are :" + Normal.normala1(arr[i].getPos() - 1));
+				System.out.println("Choose your answer.");
+				String answer = input.nextLine();
+				if (Normal.normalc1(arr[i].getPos() - 1).equals(answer)) {
+					return answ = true;
+				} else {
+					return answ = false;
+				}
+			} else if (arr[i].getPos() <= 11) {
+				System.out.println("The answers are :" + Normal.normala2(arr[i].getPos() - 7));
+				System.out.println("Choose your answer.");
+				String answer = input.nextLine();
+				if (Normal.normalc2(arr[i].getPos() - 7).equals(answer)) {
+					return answ = true;
+				} else {
+					return answ = false;
+				}
+			} else if (arr[i].getPos() <= 17) {
+				System.out.println("The answers are :" + Normal.normala3(arr[i].getPos() - 13));
+				System.out.println("Choose your answer.");
+				String answer = input.nextLine();
+				if (Normal.normalc3(arr[i].getPos() - 13).equals(answer)) {
+					return answ = true;
+				} else {
+					return answ = false;
+				}
+			} else {
+				System.out.println("The answers are :" + Normal.normala4(arr[i].getPos() - 19));
+				System.out.println("Choose your answer.");
+				String answer = input.nextLine();
+				if (Normal.normalc4(arr[i].getPos() - 19).equals(answer)) {
+					return answ = true;
+				} else {
+					return answ = false;
+				}
+			}
+		}
+		if (i == 1) {
+			if (arr[i].getPos() <= 5) {
+				System.out.println("The answers are :" + Normal.normala1(arr[i].getPos() + 4));
+				System.out.println("Choose your answer.");
+				String answer = input.nextLine();
+				if (Normal.normalc1(arr[i].getPos() + 4).equals(answer)) {
+					return answ = true;
+				} else {
+					return answ = false;
+				}
+			} else if (arr[i].getPos() <= 11) {
+				System.out.println("The answers are :" + Normal.normala2(arr[i].getPos() - 2));
+				System.out.println("Choose your answer.");
+				String answer = input.nextLine();
+				if (Normal.normalc2(arr[i].getPos() - 2).equals(answer)) {
+					return answ = true;
+				} else {
+					return answ = false;
+				}
+			} else if (arr[i].getPos() <= 17) {
+				System.out.println("The answers are :" + Normal.normala3(arr[i].getPos() - 8));
+				System.out.println("Choose your answer.");
+				String answer = input.nextLine();
+				if (Normal.normalc3(arr[i].getPos() - 8).equals(answer)) {
+					return answ = true;
+				} else {
+					return answ = false;
+				}
+			} else {
+				System.out.println("The answers are :" + Normal.normala4(arr[i].getPos() - 14));
+				System.out.println("Choose your answer.");
+				String answer = input.nextLine();
+				if (Normal.normalc4(arr[i].getPos() - 14).equals(answer)) {
+					return answ = true;
+				} else {
+					return answ = false;
+				}
+			}
+		}
+		if (i == 2) {
+			if (arr[i].getPos() <= 5) {
+				System.out.println("The answers are :" + Normal.normala1(arr[i].getPos() + 9));
+				System.out.println("Choose your answer.");
+				String answer = input.nextLine();
+				if (Normal.normalc1(arr[i].getPos() + 9).equals(answer)) {
+					return answ = true;
+				} else {
+					return answ = false;
+				}
+			} else if (arr[i].getPos() <= 11) {
+				System.out.println("The answers are :" + Normal.normala2(arr[i].getPos() + 3));
+				System.out.println("Choose your answer.");
+				String answer = input.nextLine();
+				if (Normal.normalc2(arr[i].getPos() + 3).equals(answer)) {
+					return answ = true;
+				} else {
+					return answ = false;
+				}
+			} else if (arr[i].getPos() <= 17) {
+				System.out.println("The answers are :" + Normal.normala3(arr[i].getPos() - 3));
+				System.out.println("Choose your answer.");
+				String answer = input.nextLine();
+				if (Normal.normalc3(arr[i].getPos() - 3).equals(answer)) {
+					return answ = true;
+				} else {
+					return answ = false;
+				}
+			} else {
+				System.out.println("The answers are :" + Normal.normala4(arr[i].getPos() - 9));
+				System.out.println("Choose your answer.");
+				String answer = input.nextLine();
+				if (Normal.normalc4(arr[i].getPos() - 9).equals(answer)) {
+					return answ = true;
+				} else {
+					return answ = false;
+				}
+			}
+		}
+		if (i == 3) {
+			if (arr[i].getPos() <= 5) {
+				System.out.println("The answers are :" + Normal.normala1(arr[i].getPos() + 14));
+				System.out.println("Choose your answer.");
+				String answer = input.nextLine();
+				if (Normal.normalc1(arr[i].getPos() + 14).equals(answer)) {
+					return answ = true;
+				} else {
+					return answ = false;
+				}
+			} else if (arr[i].getPos() <= 11) {
+				System.out.println("The answers are :" + Normal.normala2(arr[i].getPos() + 8));
+				System.out.println("Choose your answer.");
+				String answer = input.nextLine();
+				if (Normal.normalc2(arr[i].getPos() + 8).equals(answer)) {
+					return answ = true;
+				} else {
+					return answ = false;
+				}
+			} else if (arr[i].getPos() <= 17) {
+				System.out.println("The answers are :" + Normal.normala3(arr[i].getPos() + 2));
+				System.out.println("Choose your answer.");
+				String answer = input.nextLine();
+				if (Normal.normalc3(arr[i].getPos() + 2).equals(answer)) {
+					return answ = true;
+				} else {
+					return answ = false;
+				}
+			} else {
+				System.out.println("The answers are :" + Normal.normala4(arr[i].getPos() - 4));
+				System.out.println("Choose your answer.");
+				String answer = input.nextLine();
+				if (Normal.normalc4(arr[i].getPos() - 4).equals(answer)) {
+					return answ = true;
+				} else {
+					return answ = false;
+				}
+			}
+		}
+		return answ;
+	}
+}
