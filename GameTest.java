9945b3ce266b8ad26f0f42faf0263e691f81da3e
lamprinh <93085770+lamprinh@users.noreply.github.com>
2022-01-18 18:19:46 +0200

    Add files via upload

---
+++ GameTest.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+	static Random dicenumber = new Random();
+
+	@Test
+	void testRoll_1() {
+		int dice;
+		dice = 1 + dicenumber.nextInt(6);
+		Assert.assertTrue(dice >= 1 && dice <= 6);
+	}
+
+}
