671111934509754a228decb74607c812670a10d4
Stratis Giannakas <94313627+Stratisgi@users.noreply.github.com>
2022-01-18 23:21:10 +0200

    Add files via upload

---
+++ Easy.java
@@ -0,0 +1,294 @@
+import java.util.ArrayList;
+
+public class Easy extends Bank {
+	public static void easyQ1(int i) {
+			ArrayList<String> easy_q1 = new ArrayList<String>();
+			easy_q1.add("Thea tis oikogeneias kai tis oikiakis zois itan i Estias ");
+			easy_q1.add("Tin eisodo tou adi fulouse o minotauros");
+			easy_q1.add("I mitera tis Persefonis itan i Dimitra");
+			easy_q1.add("Pateras tou dia itan o Kronos");
+			easy_q1.add("Kai oi 12 theoi emenan ston Olumpo");
+			easy_q1.add("O ifaistos itan paidi tis iras kai tou dia");
+			easy_q1.add("To laburintho gia to basilia minoa eftiakse o iraklis");
+			easy_q1.add("O troúkos polemos kratise 10 xronia");
+			easy_q1.add("Gunaika tou adi itan i persefoni");
+			easy_q1.add("Teleutaios athlos tou irakli itan na apagagei ton kerbero apo ton adi");
+			easy_q1.add("Tous athlous tou irakli anethete o dias");
+			easy_q1.add("Vasilias tis troias kata ton troiko polemo itan o priamos");
+			easy_q1.add("Suzugos tou apollona einai i afroditi");
+			easy_q1.add("stin titanomaxia oi gigantes itan me tous olumpious");
+			easy_q1.add("theos ton oneiron itan o morfeas");
+			easy_q1.add("to muthiko plasma pou prostateue tin kriti itan o talos");
+			easy_q1.add("o promitheas itan theos");
+			easy_q1.add("idiotita tou melampoda itan mantis");
+			easy_q1.add("o pistos skulos tou odussea itan o paris");
+			easy_q1.add("o okeanos itan titanas");
+			System.out.println(easy_q1.get(i) + " dektes apantiseis mono s / l");
+		}
+
+		public static void easyQ2(int i) {
+			ArrayList<String> easy_q2 = new ArrayList<String>();
+			easy_q2.add("anotatos arxontas tou mukinaúkou kosmou itan o basilias ");
+			easy_q2.add(
+					"o autokratoras pou edose oristiko telos sti leitourgeia tou manteiou ton delfon me diatagma itan o theodosios a’ ");
+			easy_q2.add("pateras tou kserksi itan o dareios a’");
+			easy_q2.add("oi triakonta turannoi kubernisan stin athina gia duo xronia. ");
+			easy_q2.add(
+					"o tragodos pou zitise na xaraksoun ston tafo tou to epigramma gia ti maxi tou marathona itan o aisxulos. ");
+			easy_q2.add("stratigos ton person sti maxi ton plataion itan o kserksis. ");
+			easy_q2.add("o kleisthenis metetrepse ti bouli ton tetrakosion se bouli ton pentakosion stin arxaia athina. ");
+			easy_q2.add("i iliaia itan to laúko dikastirio. ");
+			easy_q2.add(
+					"o efialtis itan athinaios politikos ekprosopos tis aristokratikis parataksis kai edose polla pronomia stous aristokrates. ");
+			easy_q2.add(
+					"i athina eftase sto apogeio tis politikis kai politistikis anaptuksis ton 5o aiona p.x. me igeti ton perikli. ");
+			easy_q2.add("kuriarxo rolo sti minoiki theologia eixe o dias. ");
+			easy_q2.add(
+					"sto minoiko kosmo xrisimopoiithikan parallila mia ieroglufiki grafi kai mia grafi grammika sumbola gnosti os grammiki a’. ");
+			easy_q2.add("anotatos arxontas tou mukinaúkou kosmou itan o basileus. ");
+			easy_q2.add(
+					"kata to deutero elliniko apoikismo oi ellines eksaplothikan sti mesogeio kai ston eukseino ponto, sta oria tou tote gnostou kosmou. ");
+			easy_q2.add(
+					"apotelesmata tou peloponnisiakou polemou itan i itta ton spartiaton kai kat’ epektasi i anagnorisi tis athinaúkis igemonias apo tis ellinikes poleis. ");
+			easy_q2.add("i grammiki b’ grafi den exei apokruptografithei akomi. ");
+			easy_q2.add(
+					"kata ton proto elliniko apoikismo ta ellinika fula eksaplothikan me gefura ta nisia tou aigaiou stis dutikes aktes tis m. asias.");
+			easy_q2.add("o megas aleksandros eftase mexri to gaggi pou apotelese kai to orio tis ekstrateias tou. ");
+			easy_q2.add("o megas aleksandros pelekise gia proti fora tous perses sto graniko. ");
+			easy_q2.add("o peloponnisiakos polemos diirkise eikosi xronia. ");
+			System.out.println(easy_q2.get(i) + " dektes apantiseis mono s / l");
+		}
+
+		public static void easyQ3(int i) {
+			ArrayList<String> easy_q3 = new ArrayList<String>();
+			easy_q3.add("i epanastasi ksekinise 25 martiou 1821.");
+			easy_q3.add("prin tin epanastasi tis 25is upirksan k alles alla apetuxan.");
+			easy_q3.add("oi ellines kerdisan ti maxi ton derbenakion.");
+			easy_q3.add("o th. kolokotronis apotelei mia polu simantiki prosopikotita tis epanastasis.");
+			easy_q3.add("i maxi tis grabias dothike stis 8 maÀou 1821.");
+			easy_q3.add("o k. kanaris pethane 2/9/1878.");
+			easy_q3.add("i apeleutherosi tis kalamatas den egine to septembrio tou 1821.");
+			easy_q3.add("i katalipsi tis tripolitsas egine to septembrio tou 1821.");
+			easy_q3.add("i naumaxia tou nabarinou egine to septembrio tou 1827.");
+			easy_q3.add("i a. miaoulis pethane 11 iouliou 1835.");
+			easy_q3.add("i a. miaoulis gennithike 20 maÀou 1769.");
+			easy_q3.add("i tripolitsas sumfona me ton kolokotroni kateixe stratigiki thesi.");
+			easy_q3.add("i alosi tis tripolitsas apotelese stathmo gia tin edraiosi tis epanastasis.");
+			easy_q3.add("i ptosi tou mesologgiou egine ton aprilio tou 1826.");
+			easy_q3.add("i sundromi ton megalon dunameon stin apeleutherosi upirkse kathoristiki.");
+			easy_q3.add("o g. karaúskakis entaxthike sta 15 se kleftiki omada.");
+			easy_q3.add("o g. karaúskakis pethane 27/2/1827.");
+			easy_q3.add("oi ellines ittithikan sti maxi tou analatou.");
+			easy_q3.add("o karaúskakis dioristike arxistratigos tis tripolitsas.");
+			easy_q3.add("georgios dikaios flesas itan to pragmatiko onoma tou papaflessa.");
+			System.out.println(easy_q3.get(i) + " dektes apantiseis mono s / l");
+		}
+
+		public static void easyQ4(int i) {
+			ArrayList<String> easy_q4 = new ArrayList<String>();
+			easy_q4.add("o a pagkosmios polemos ksekinise to 1914.");
+			easy_q4.add("o a pagkosmios polemos elikse to 1918.");
+			easy_q4.add("o ellinoútalikos polemos ksekinise to 1940.");
+			easy_q4.add("oi germanikes dunameis epitethikan stin ellada stis 6/5/1941.");
+			easy_q4.add("o ioannis metaksas gennithike stin ithaki.");
+			easy_q4.add("o metaksas aperripse to telesigrafo tou mousolini tin 28/10/1940.");
+			easy_q4.add("o mousolini upirkse summaxos tou xitler.");
+			easy_q4.add("o mousolini ektelestike to 1945 apo ellines antartes.");
+			easy_q4.add("i katastrofi tis smurnis egine to septembrio tou 1922.");
+			easy_q4.add("i katastrofi tis smurnis arxise meta tin eisodo tou tourkikou stratou kai tou kemal.");
+			easy_q4.add("o mikrasiatikos polemos diirkise apo to 1919 eos 1922 ");
+			easy_q4.add("i sunthiki tis lozanis upograftike 24/7/1925.");
+			easy_q4.add("o moustafa kemal theoreitai dimiourgos tis sugxronis tourkias.");
+			easy_q4.add("mia apo tis sunepeies tou a’ pagkosmiou polemou itan i dimiourgia pollon meionotiton.");
+			easy_q4.add("i sunthiki ton sebron upograftike 28/6/1922.");
+			easy_q4.add("i sunthiki ton bersallion upograftike 28/6/1916.");
+			easy_q4.add("ti protoboulia ekthronisis tou basilia kostantinou anelabe i agglia.");
+			easy_q4.add("o el. benizelos pethane 10 martiou 1939.");
+			easy_q4.add("oi tafoi ton benizelon apoteloun aksiotheato ton xanion.");
+			easy_q4.add("o el. benizelos pethane sto parisi se ilikia 75 eton.");
+			System.out.println(easy_q4.get(i) + " dektes apantiseis mono s / l");
+		}
+
+		public static String easyc1(int i) {
+			ArrayList<String> easy_correct1 = new ArrayList<String>();
+			easy_correct1.add("s");
+			easy_correct1.add("l");
+			easy_correct1.add("s");
+			easy_correct1.add("s");
+			easy_correct1.add("l");
+			easy_correct1.add("l");
+			easy_correct1.add("l");
+			easy_correct1.add("s");
+			easy_correct1.add("s");
+			easy_correct1.add("s");
+			easy_correct1.add("l");
+			easy_correct1.add("s");
+			easy_correct1.add("l");
+			easy_correct1.add("s");
+			easy_correct1.add("s");
+			easy_correct1.add("s");
+			easy_correct1.add("l");
+			easy_correct1.add("s");
+			easy_correct1.add("l");
+			easy_correct1.add("s");
+			return easy_correct1.get(i);
+		}
+
+		public static String easyc2(int i) {
+			ArrayList<String> easy_correct2 = new ArrayList<String>();
+			easy_correct2.add("l");
+			easy_correct2.add("s");
+			easy_correct2.add("s");
+			easy_correct2.add("l");
+			easy_correct2.add("s");
+			easy_correct2.add("l");
+			easy_correct2.add("s");
+			easy_correct2.add("s");
+			easy_correct2.add("l");
+			easy_correct2.add("s");
+			easy_correct2.add("l");
+			easy_correct2.add("s");
+			easy_correct2.add("l");
+			easy_correct2.add("s");
+			easy_correct2.add("l");
+			easy_correct2.add("l");
+			easy_correct2.add("s");
+			easy_correct2.add("l");
+			easy_correct2.add("s");
+			easy_correct2.add("l");
+			return easy_correct2.get(i);
+		}
+
+		public static String easyc3(int i) {
+			ArrayList<String> easy_correct3 = new ArrayList<String>();
+			easy_correct3.add("s");
+			easy_correct3.add("s");
+			easy_correct3.add("s");
+			easy_correct3.add("s");
+			easy_correct3.add("s");
+			easy_correct3.add("l");
+			easy_correct3.add("s");
+			easy_correct3.add("s");
+			easy_correct3.add("l");
+			easy_correct3.add("l");
+			easy_correct3.add("l");
+			easy_correct3.add("s");
+			easy_correct3.add("s");
+			easy_correct3.add("s");
+			easy_correct3.add("s");
+			easy_correct3.add("s");
+			easy_correct3.add("l");
+			easy_correct3.add("s");
+			easy_correct3.add("l");
+			easy_correct3.add("s");
+			return easy_correct3.get(i);
+		}
+
+		public static String easyc4(int i) {
+			ArrayList<String> easy_correct4 = new ArrayList<String>();
+			easy_correct4.add("s");
+			easy_correct4.add("s");
+			easy_correct4.add("s");
+			easy_correct4.add("l");
+			easy_correct4.add("s");
+			easy_correct4.add("s");
+			easy_correct4.add("s");
+			easy_correct4.add("l");
+			easy_correct4.add("s");
+			easy_correct4.add("s");
+			easy_correct4.add("s");
+			easy_correct4.add("s");
+			easy_correct4.add("l");
+			easy_correct4.add("s");
+			easy_correct4.add("l");
+			easy_correct4.add("s");
+			easy_correct4.add("l");
+			easy_correct4.add("l");
+			easy_correct4.add("s");
+			easy_correct4.add("l");
+			return easy_correct4.get(i);
+	}
+
+	// public static void Showquestion(int i, Player[] arr1) {
+	// String question = Game.roll_easy(i, arr1);
+	// System.out.println(question);
+	// }
+
+	@Override
+	public void giveMoney(int i, Player[] arr) {
+		if (Bank.King_area[arr[i].getPos()] == 9) {
+			System.out.println("H perioxi den anikei se kanenan.");
+			int a = -1 * Bank.getAmount();
+			arr[i].setWallet(Bank.getAmount());
+			Bank.setMoney(a);
+			System.out.println("To portofoli tou " + arr[i].getCharacter() + " exei " + arr[i].getWallet() + " euro.");
+			buyProperty(i, arr);
+		} else {
+			int x = Bank.King_area[arr[i].getPos()];
+			System.out.println("H perioxi anikei ston " + arr[x].getCharacter());
+			transferTrue(i, arr);
+		}
+	}
+
+	@Override
+	public void takeMoney(int i, Player[] arr) {
+		if (Bank.King_area[arr[i].getPos()] == 9) {
+			System.out.println("H perioxi den anikei se kanenan.");
+			int a = Bank.getAmount();
+			arr[i].setWallet(-Bank.getAmount());
+			Bank.setMoney(a);
+			System.out.println("To portofoli tou " + arr[i].getCharacter() + "meiothike kata " + Bank.getAmount()
+					+ "euro kai pleon exei sto portofoli tou " + arr[i].getWallet() + " euro.");
+		} else {
+			int x = Bank.King_area[arr[i].getPos()];
+			System.out.println("H perioxi anikei ston " + arr[x].getCharacter());
+			transferFalse(i, arr);
+		}
+	}
+
+	@Override
+	public void transferTrue(int i, Player[] arr) {
+		if (Bank.King_area[arr[i].getPos()] != 9) {
+			int z = Bank.King_area[arr[i].getPos()];
+			System.out.println("To portofoli tou " + arr[i].getCharacter() + " meiothike kata " + (2 * Bank.getAmount())
+					+ "åõñþ êáé ðëÝïí Ý÷åé óôï ðïñôïöüëé ôïõ" + arr[i].getWallet() + "åõñþ");
+			arr[i].setWallet(-2 * Bank.getAmount());
+			arr[z].setWallet(2 * Bank.getAmount());
+			System.out.println("To portofoli tou " + arr[z].getCharacter() + "afksithike kata " + (2 * Bank.getAmount())
+					+ " euro kai pleon exei sto portofoli tou " + arr[z].getWallet() + " euro.");
+		}
+	}
+
+	@Override
+	public void transferFalse(int i, Player[] arr) {
+		if (Bank.King_area[arr[i].getPos()] != 9) {
+			int x = (int) 0.75 * Bank.getAmount();
+			int z = Bank.King_area[arr[i].getPos()];
+			System.out.println("To portofoli tou " + arr[i].getCharacter() + " meiothike kata " + (0.75 * Bank.getAmount())
+					+ " euro kai pleon exei sto portofoli tou " + arr[i].getWallet() + " euro.");
+			arr[i].setWallet(-x);
+			arr[z].setWallet(x);
+			System.out.println("To portofoli tou " + arr[z].getCharacter() + " afksithike kata " + (0.75 * Bank.getAmount())
+					+ " euro kai pleon exei sto portofoli tou " + arr[z].getWallet() + " euro. ");
+		}
+	}
+
+	@Override
+	public void buyProperty(int i, Player[] arr) {
+		if (Bank.King_area[arr[i].getPos()] == 9) {
+			if (Game.buy() == true) {
+				int a = -Bank.Price[arr[i].getPos()];
+				arr[i].setWallet(-Bank.Price[arr[i].getPos()]);
+				System.out.println(
+						"To portofoli tou " + arr[i].getCharacter() + "meiothike kata " + Bank.Price[arr[i].getPos()]
+								+ " euro kai pleon exei sto portofoli tou " + arr[i].getWallet() + " euro.");
+				Bank.setMoney(a);
+				Bank.King_area[arr[i].getPos()] = i;
+				System.out.println(
+						"H perioxi " + Bank.Prop[arr[i].getPos()] + " anikei pleon ston " + arr[i].getCharacter());
+			} else {
+				System.out.println("O paiktis " + arr[i].getCharacter() + " apofasise na min agorasei tin perioxi.");
+			}
+		}
+	}
+}
---
+++ Menucards.java
@@ -0,0 +1,245 @@
+import java.util.Scanner;
+
+public class Menucards {
+	public static void Menu1(int amount, Player[] arr, int i) {
+		System.out.println(+ arr[i].getCharacter() +" Pare 500 euro ap tous upoloipous paiktes.");
+		arr[i].getWallet();
+		arr[i].setWallet(3 * amount);
+		Player p = arr[i];
+		for (int j = 0; j < arr.length; j++) {
+			if (arr[j] != p) {
+				arr[j].getWallet();
+				arr[j].setWallet(-amount);
+				System.out.println("O παίκτης  " + arr[j].getCharacter() + "  exei meiosi sto portofoli tou kata  "
+						+ amount + " euro kai pleon exei " + arr[j].getWallet() + " euro.");
+			}
+		}
+		System.out.println("O paiktis  " + p.getCharacter() + "  exei afksisi sto portofoli tou kata " + 3 * amount
+				+ " euro kai pleon exei " + p.getWallet() + " euro.");
+	}
+
+	public static void Menu2(int i, Player[] arr) {
+		System.out.println(+ arr[i].getCharacter() + " Metakinisou 3 theseis piso.");
+		Player p = arr[i];
+		p.getPos();
+		p.setPos(-3);
+		System.out.println( + p.getCharacter() + " briskesai stin thesi " + p.getPos() + ".");
+	}
+
+	public static void Menu3(int pos, Player[] arr, int i) {
+		System.out.println(+ arr[i].getCharacter() + " Pigaine stous delfous. An anikei se kapoion i perioxi tou ofeileis 500 euro. An peraseis apo tin afetiria sou afairountai 1000euro.");
+		Player p = arr[i];
+		int f_pos = p.getPos();
+		p.setPos(pos - f_pos);
+		int l_pos = p.getPos();
+		if (pos > f_pos) {
+			System.out.println("O paiktis  " + p.getCharacter() + " brisketai stin thesi " + p.getPos() + ".");
+		} else if (pos < f_pos) {
+			p.setWallet(-1000);
+			System.out.println("O paiktis  " + p.getCharacter()
+					+ " pernaei apo tin afetiria alla tou afairountai ta 1000 euro pou dikaioutai.");
+			System.out.println("O paiktis  " + p.getCharacter() + " brisketai stin thesi " + p.getPos() + ".");
+		}
+		for (int j = 0; j < arr.length; j++) {
+			if (Bank.King_area[l_pos] == j && i != j) {
+				p.setWallet(-500);
+				arr[j].setWallet(500);
+				System.out.println("Afaireitai apo ton paikti " + p.getCharacter()
+						+ " to poso twn 500euro kai metaferetai ston paikti  " + arr[j].getCharacter()
+						+ " . H aksia twn portofoliwn tous isoutai me " + p.getWallet() + arr[j].getWallet()
+						+ " antistoixa.");
+			}
+		}
+	}
+
+	public static void Menu4(int amount, Player[] arr, int j) {
+		System.out.println(+ arr[i].getCharacter() + " Afksithikan oi aksies twn perioxwn pou katexeis pare 250 euro.");
+		for (int i = 0; i < Bank.King_area.length; i++) {
+			if (Bank.King_area[i] == j) {
+				arr[j].setWallet(amount);
+				Bank.setMoney(-amount);
+				System.out.println(" O paiktis " + arr[j].getCharacter() + " pairnei " + amount + " euro kai pleon exei "
+						+ arr[j].getWallet() + " euro.");
+				break;
+			}
+		}
+	}
+
+	public static void Menu5(int amount, Player[] arr, int j, int k) {
+		System.out.println(+ arr[i].getCharacter() +"An o epomenos paiktis katexei perissoteres perioxes tou ofeileis 500 euro. Diaforetika sou ofeilei 500 euro.");
+		int counterp = 0;
+		int counterp1 = 0;
+		for (int i = 0; i < Bank.King_area.length; i++) {
+			if (Bank.King_area[i] == j) {
+				counterp++;
+			} else if (Bank.King_area[i] == k) {
+				counterp1++;
+			}
+		}
+		if (counterp > counterp1) {
+			arr[j].setWallet(amount);
+			arr[k].setWallet(-amount);
+			System.out.println("O paiktis  " + arr[j].getCharacter() + " katexei perissoteres perioxes apo ton paikti "
+					+ arr[k].getCharacter() + " kai afksanetai to portofoli tou kata " + amount + " euro. Pleon exei "
+					+ arr[j].getWallet());
+			System.out.println("O paiktis  " + arr[k].getCharacter() + " katexei ligoteres perioxes apo ton paikti "
+					+ arr[j].getCharacter() + " kai meionetai to portofoli tou kata " + amount + " euro. Pleon exei "
+					+ arr[k].getWallet());
+		} else if (counterp < counterp1) {
+			arr[j].setWallet(-amount);
+			arr[k].setWallet(amount);
+			System.out.println("O paiktis  " + arr[k].getCharacter() + " katexei perissoteres perioxes apo ton paikti "
+					+ arr[j].getCharacter() + "  kai afksanetai to portofoli tou kata " + amount + " euro. Pleon exei "
+					+ arr[k].getWallet());
+			System.out.println("O paiktis " + arr[j].getCharacter() + " katexei ligoteres perioxes apo ton paikti "
+					+ arr[k].getCharacter() + " kai meionetai to portofoli tou kata " + amount + " euro. Pleon exei "
+					+ arr[j].getWallet());
+
+		}
+	}
+
+	public static void Menu7(Player[] arr, int i) {
+		System.out.println(+arr[i].getCharacter() +" Kinisou 2 theseis mprosta.");
+		arr[i].setPos(2);
+		System.out.println("O paiktis " + arr[i].getCharacter() + " brisketai stin thesi " + arr[i].getPos() + ".");
+	}
+
+	public static void Menu8(int i, Player[] arr) {
+		System.out.println(+ arr[i].getCharacter() +" An to epipedo duskolias einai eukolo sou afairountai 5000 euro.");
+		if (Game.levelfinder(i, arr).equals("easy")) {
+			arr[i].setWallet(-5000);
+			Bank.setMoney(5000);
+			System.out.println(" To portofoli tou paikti " + arr[i].getCharacter()
+					+ " meiothike kata 5000 euro kai pleon isoutai me " + arr[i].getWallet() + " ευρώ.");
+		} else {
+			System.out.println("O paiktis " + arr[i].getCharacter() + " den brisketai sto eukolo epipedo.");
+		}
+	}
+
+	public static void Menu9(int i, Player[] arr) {
+		System.out.println(+ arr[i].getCharacter() + " An to epipedo duskolias einai metrio ofeileis 5000 euro se enan apo tous paiktes. Dikaiousai na dialekseis.");
+		Scanner input = new Scanner(System.in);
+		if (Game.levelfinder(i, arr).equals("normal")) {
+			System.out.println("Dialekse ton paikti.");
+			int z = input.nextInt();
+			arr[i].setWallet(-5000);
+			arr[z].setWallet(5000);
+			System.out.println(" O paiktis  " + arr[z].getCharacter() + " pairnei 5000 euro kai pleon exei "
+					+ arr[z].getWallet() + " euro.");
+			System.out.println(" O paiktis   " + arr[i].getCharacter() + " xanei 5000 euro kai pleon exei "
+					+ arr[i].getWallet() + " euro.");
+		} else {
+			System.out.println("O paiktis" + arr[i].getCharacter() + " den brisketai sto metrio epipedo.");
+
+		}
+	}
+
+	public static void Menu10(int x, int[] King_area, Player[] arr) {
+		System.out.println(+ arr[i].getCharacter() + " An to epipedo duskolias einai duskolo kai katexeis 5 i perissoteres perioxes xaneis 1000 euro. Allios kerdizeis 1000 euro.");
+		if (Game.levelfinder(x, arr).equals("hard")) {
+			int belong = 0;
+			for (int i = 0; i < King_area.length; i++) {
+				if (King_area[i] != 9) {
+					if (King_area[i] == x) {
+						belong = belong += 1;
+					}
+				}
+			}
+			if (belong >= 5) {
+				arr[x].setWallet(-1000);
+				Bank.setMoney(1000);
+				System.out.println("O paiktis " + arr[x].getCharacter() + " xanei apo to portofoli tou " + 1000 + " euro.");
+			} else {
+				arr[x].setWallet(1000);
+				Bank.setMoney(-1000);
+				System.out.println( "O paiktis " + arr[x].getCharacter() + " prosthetei sto portofoli tou " + 1000 + " euro.");
+			}
+		} else {
+			System.out.println("O paiktis " + arr[x].getCharacter() + " den brisketai sto duskolo epipedo.");
+		}
+	}
+
+	public static void Menu11(Player[] arr, int k) {
+		System.out.println(+arr[k].getCharacter() + " Dikaiousai na agoraseis mia mi katexomeni perioxi xwris na apantiseis kapoia erotisi.");
+		Player p = arr[k];
+		boolean flag = false;
+		Scanner input = new Scanner(System.in);
+		int j = 0;
+		int[] B = new int[20];
+		for (int i = 0; i < Bank.King_area.length; i++) {
+			if (Bank.King_area[i] == 9) {
+				B[j] = i;
+				flag = true;
+				j++;
+			}
+		}
+		if (flag) {
+			System.out.println("Oi parakato perioxes einai diathesimes.");
+			for (int i = 0; i < B.length; i++) {
+				System.out.println(+B[i]);
+			}
+		} else {
+			System.out.println("Den yparxei diathesimi perioxi.");
+		}
+		System.out.println("Tha itheles na agoraseis kapoia perioxi;");
+		String answer = input.nextLine();
+		if (answer.equals("YES")) {
+			System.out.println("Dialekse poia apo tis diathesimes perioxes thes na agoraseis.");
+			int area = input.nextInt();
+			p.setWallet(-Bank.Price[area]);
+			Bank.King_area[area] = k;
+			Bank.setMoney(Bank.Price[area]);
+			System.out.println(
+					"O paiktis " + p.getCharacter() + " xanei apo to portofoli tou " + Bank.Price[area] + " euro");
+			System.out.println("O paiktis " + p.getCharacter() + " agorase tin perioxi " + Bank.Prop[area] + ".");
+		} else {
+			System.out.println("O paiktis " + p.getCharacter() + " apofasise na min agorasei kapoia perioxi.");
+		}
+	}
+
+	public static void Menu6(Player[] arr, int i, int amount) {
+		System.out.println(+ arr[i].getCharacter() + " Dose 600 euro stous upoloipous paiktes.");
+		Player p = arr[i];
+		for (int j = 0; j < arr.length; j++) {
+			if (j != i) {
+				p.setWallet(-amount);
+				System.out.println("O paiktis " + p.getCharacter() + "xanei apo to portofoli tou " + amount + " euro ");
+				arr[j].setWallet(amount);
+				System.out.println(
+						"O paiktis" + arr[j].getCharacter() + " prosthetei sto portofoli tou " + amount + " euro ");
+			}
+		}
+	}
+
+	public static void Menu12(Player[] arr, int i, int amount, int sum) {
+		System.outprintln("An katexeis panw apo 4 perioxes pairneis 700 euro apo tous upoloipous paiktes. Diaforetika dineis 700 euro stous upoloipous paiktes.");
+		Player p = arr[i];
+		int counter = 0;
+		for (int m = 0; i < Bank.King_area.length; i++) {
+			if (Bank.King_area[m] == i) {
+				counter++;
+			}
+		}
+		if (counter >= sum) {
+			for (int j = 0; j < arr.length; j++) {
+				if (j != i) {
+					p.setWallet(amount);
+					arr[j].setWallet(-amount);
+					System.out.println(
+							"O paiktis " + p.getCharacter() + " prosthetei sto portofoli tou " + amount + " euro.");
+					System.out.println("O paiktis " + arr[j].getCharacter() + " xanei " + amount + " euro.");
+				}
+			}
+		} else {
+			for (int j = 0; j < arr.length; j++) {
+				p.setWallet(-amount);
+				arr[j].setWallet(amount);
+				System.out.println(
+						"O paiktis " + arr[j].getCharacter() + " prosthetei sto portofoli tou " + amount + " euro.");
+				System.out.println("O paiktis " + p.getCharacter() + " xanei " + amount + " euro.");
+
+			}
+
+		}
+	}
+}
