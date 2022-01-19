01bb13b22cd7188944a8380fef21514897873fc3
efstdafnakis <93093067+efstdafnakis@users.noreply.github.com>
2022-01-19 14:17:19 +0200

    Update Normal_answer.java

--- Normal_answer.java
+++ Normal_answer.java
@@ -1,18 +1,25 @@
+package paketolamp;
+
 import java.util.Scanner;
 
 public class Normal_answer {
 	static Scanner input = new Scanner(System.in);
 
-	
-	public static boolean show_answer(int i, Player[] arr) { 	//Emfanizei tis epiloges gia tin apantisi tis erotisis, sto deutero epipedo diskolias 
-																//Diavazei tin apanthsh kai thn elegxei
-																//gia ton kathe paikti
+	public static boolean show_answer(int i, Player[] arr) { // Emfanizei tis epiloges gia tin apantisi tis erotisis,
+																// sto deutero epipedo diskolias
+																// Diavazei tin apanthsh kai thn elegxei
+																// gia ton kathe paikti
 		boolean answ = false;
 		if (i == 0) {
 			if (arr[i].getPos() <= 5) {
 				System.out.println("Oi epiloges einai : " + Normal.normala1(arr[i].getPos() - 1));
 				System.out.println("Epelexe tin apantisi sou.");
 				String answer = input.nextLine();
+				while (answer.compareTo("a")!=0 && answer.compareTo("b")!=0 && answer.compareTo("g")!=0) {
+					System.out.println("Edwses mi diathesimi apantisi. Dektes apantiseis mono (a/b/g).");
+					System.out.println("Apantise ksana");
+					answer = input.nextLine();
+				}
 				if (Normal.normalc1(arr[i].getPos() - 1).equals(answer)) {
 					return answ = true;
 				} else {
@@ -22,6 +29,11 @@ public class Normal_answer {
 				System.out.println("Oi epiloges einai : " + Normal.normala2(arr[i].getPos() - 7));
 				System.out.println("Epelexe tin apantisi sou.");
 				String answer = input.nextLine();
+				while (answer.compareTo("a")!=0 && answer.compareTo("b")!=0 && answer.compareTo("g")!=0) {
+									System.out.println("Edwses mi diathesimi apantisi. Dektes apantiseis mono (a/b/g).");
+									System.out.println("Apantise ksana");
+									answer = input.nextLine();
+				}
 				if (Normal.normalc2(arr[i].getPos() - 7).equals(answer)) {
 					return answ = true;
 				} else {
@@ -31,6 +43,11 @@ public class Normal_answer {
 				System.out.println("Oi epiloges einai : " + Normal.normala3(arr[i].getPos() - 13));
 				System.out.println("Epelexe tin apantisi sou.");
 				String answer = input.nextLine();
+				while (answer.compareTo("a")!=0 && answer.compareTo("b")!=0 && answer.compareTo("g")!=0) {
+									System.out.println("Edwses mi diathesimi apantisi. Dektes apantiseis mono (a/b/g).");
+									System.out.println("Apantise ksana");
+									answer = input.nextLine();
+				}
 				if (Normal.normalc3(arr[i].getPos() - 13).equals(answer)) {
 					return answ = true;
 				} else {
@@ -40,6 +57,11 @@ public class Normal_answer {
 				System.out.println("Oi epiloges einai : " + Normal.normala4(arr[i].getPos() - 19));
 				System.out.println("Epelexe tin apantisi sou.");
 				String answer = input.nextLine();
+				while (answer.compareTo("a")!=0 && answer.compareTo("b")!=0 && answer.compareTo("g")!=0) {
+									System.out.println("Edwses mi diathesimi apantisi. Dektes apantiseis mono (a/b/g).");
+									System.out.println("Apantise ksana");
+									answer = input.nextLine();
+				}
 				if (Normal.normalc4(arr[i].getPos() - 19).equals(answer)) {
 					return answ = true;
 				} else {
@@ -52,6 +74,11 @@ public class Normal_answer {
 				System.out.println("Oi epiloges einai :" + Normal.normala1(arr[i].getPos() + 4));
 				System.out.println("Epelexe tin apantisi sou.");
 				String answer = input.nextLine();
+				while (answer.compareTo("a")!=0 && answer.compareTo("b")!=0 && answer.compareTo("g")!=0) {
+									System.out.println("Edwses mi diathesimi apantisi. Dektes apantiseis mono (a/b/g).");
+									System.out.println("Apantise ksana");
+									answer = input.nextLine();
+				}
 				if (Normal.normalc1(arr[i].getPos() + 4).equals(answer)) {
 					return answ = true;
 				} else {
@@ -61,6 +88,11 @@ public class Normal_answer {
 				System.out.println("Oi epiloges einai : " + Normal.normala2(arr[i].getPos() - 2));
 				System.out.println("Epelexe tin apantisi sou.");
 				String answer = input.nextLine();
+				while (answer.compareTo("a")!=0 && answer.compareTo("b")!=0 && answer.compareTo("g")!=0) {
+									System.out.println("Edwses mi diathesimi apantisi. Dektes apantiseis mono (a/b/g).");
+									System.out.println("Apantise ksana");
+									answer = input.nextLine();
+				}
 				if (Normal.normalc2(arr[i].getPos() - 2).equals(answer)) {
 					return answ = true;
 				} else {
@@ -70,6 +102,11 @@ public class Normal_answer {
 				System.out.println("Oi epiloges einai : " + Normal.normala3(arr[i].getPos() - 8));
 				System.out.println("Epelexe tin apantisi sou.");
 				String answer = input.nextLine();
+				while (answer.compareTo("a")!=0 && answer.compareTo("b")!=0 && answer.compareTo("g")!=0) {
+									System.out.println("Edwses mi diathesimi apantisi. Dektes apantiseis mono (a/b/g).");
+									System.out.println("Apantise ksana");
+									answer = input.nextLine();
+				}
 				if (Normal.normalc3(arr[i].getPos() - 8).equals(answer)) {
 					return answ = true;
 				} else {
@@ -79,6 +116,11 @@ public class Normal_answer {
 				System.out.println("Oi epiloges einai : " + Normal.normala4(arr[i].getPos() - 14));
 				System.out.println("Epelexe tin apantisi sou.");
 				String answer = input.nextLine();
+				while (answer.compareTo("a")!=0 && answer.compareTo("b")!=0 && answer.compareTo("g")!=0) {
+									System.out.println("Edwses mi diathesimi apantisi. Dektes apantiseis mono (a/b/g).");
+									System.out.println("Apantise ksana");
+									answer = input.nextLine();
+				}
 				if (Normal.normalc4(arr[i].getPos() - 14).equals(answer)) {
 					return answ = true;
 				} else {
@@ -91,6 +133,11 @@ public class Normal_answer {
 				System.out.println("Oi epiloges einai : " + Normal.normala1(arr[i].getPos() + 9));
 				System.out.println("Epelexe tin apantisi sou.");
 				String answer = input.nextLine();
+				while (answer.compareTo("a")!=0 && answer.compareTo("b")!=0 && answer.compareTo("g")!=0) {
+									System.out.println("Edwses mi diathesimi apantisi. Dektes apantiseis mono (a/b/g).");
+									System.out.println("Apantise ksana");
+									answer = input.nextLine();
+				}
 				if (Normal.normalc1(arr[i].getPos() + 9).equals(answer)) {
 					return answ = true;
 				} else {
@@ -100,6 +147,11 @@ public class Normal_answer {
 				System.out.println("Oi epiloges einai : " + Normal.normala2(arr[i].getPos() + 3));
 				System.out.println("Epelexe tin apantisi sou.");
 				String answer = input.nextLine();
+				while (answer.compareTo("a")!=0 && answer.compareTo("b")!=0 && answer.compareTo("g")!=0) {
+									System.out.println("Edwses mi diathesimi apantisi. Dektes apantiseis mono (a/b/g).");
+									System.out.println("Apantise ksana");
+									answer = input.nextLine();
+				}
 				if (Normal.normalc2(arr[i].getPos() + 3).equals(answer)) {
 					return answ = true;
 				} else {
@@ -109,6 +161,11 @@ public class Normal_answer {
 				System.out.println("Oi epiloges einai : " + Normal.normala3(arr[i].getPos() - 3));
 				System.out.println("Epelexe tin apantisi sou.");
 				String answer = input.nextLine();
+				while (answer.compareTo("a")!=0 && answer.compareTo("b")!=0 && answer.compareTo("g")!=0) {
+									System.out.println("Edwses mi diathesimi apantisi. Dektes apantiseis mono (a/b/g).");
+									System.out.println("Apantise ksana");
+									answer = input.nextLine();
+				}
 				if (Normal.normalc3(arr[i].getPos() - 3).equals(answer)) {
 					return answ = true;
 				} else {
@@ -118,6 +175,11 @@ public class Normal_answer {
 				System.out.println("Oi epiloges einai : " + Normal.normala4(arr[i].getPos() - 9));
 				System.out.println("Epelexe tin apantisi sou.");
 				String answer = input.nextLine();
+				while (answer.compareTo("a")!=0 && answer.compareTo("b")!=0 && answer.compareTo("g")!=0) {
+									System.out.println("Edwses mi diathesimi apantisi. Dektes apantiseis mono (a/b/g).");
+									System.out.println("Apantise ksana");
+									answer = input.nextLine();
+				}
 				if (Normal.normalc4(arr[i].getPos() - 9).equals(answer)) {
 					return answ = true;
 				} else {
@@ -130,6 +192,11 @@ public class Normal_answer {
 				System.out.println("Oi epiloges einai : " + Normal.normala1(arr[i].getPos() + 14));
 				System.out.println("Epelexe tin apantisi sou.");
 				String answer = input.nextLine();
+				while (answer.compareTo("a")!=0 && answer.compareTo("b")!=0 && answer.compareTo("g")!=0) {
+									System.out.println("Edwses mi diathesimi apantisi. Dektes apantiseis mono (a/b/g).");
+									System.out.println("Apantise ksana");
+									answer = input.nextLine();
+				}
 				if (Normal.normalc1(arr[i].getPos() + 14).equals(answer)) {
 					return answ = true;
 				} else {
@@ -139,6 +206,11 @@ public class Normal_answer {
 				System.out.println("Oi epiloges einai : " + Normal.normala2(arr[i].getPos() + 8));
 				System.out.println("Epelexe tin apantisi sou.");
 				String answer = input.nextLine();
+				while (answer.compareTo("a")!=0 && answer.compareTo("b")!=0 && answer.compareTo("g")!=0) {
+									System.out.println("Edwses mi diathesimi apantisi. Dektes apantiseis mono (a/b/g).");
+									System.out.println("Apantise ksana");
+									answer = input.nextLine();
+				}
 				if (Normal.normalc2(arr[i].getPos() + 8).equals(answer)) {
 					return answ = true;
 				} else {
@@ -148,6 +220,11 @@ public class Normal_answer {
 				System.out.println("Oi epiloges einai : " + Normal.normala3(arr[i].getPos() + 2));
 				System.out.println("Epelexe tin apantisi sou.");
 				String answer = input.nextLine();
+				while (answer.compareTo("a")!=0 && answer.compareTo("b")!=0 && answer.compareTo("g")!=0) {
+									System.out.println("Edwses mi diathesimi apantisi. Dektes apantiseis mono (a/b/g).");
+									System.out.println("Apantise ksana");
+									answer = input.nextLine();
+				}
 				if (Normal.normalc3(arr[i].getPos() + 2).equals(answer)) {
 					return answ = true;
 				} else {
@@ -157,6 +234,11 @@ public class Normal_answer {
 				System.out.println("Oi epiloges einai : " + Normal.normala4(arr[i].getPos() - 4));
 				System.out.println("Epelexe tin apantisi sou.");
 				String answer = input.nextLine();
+				while (answer.compareTo("a")!=0 && answer.compareTo("b")!=0 && answer.compareTo("g")!=0) {
+									System.out.println("Edwses mi diathesimi apantisi. Dektes apantiseis mono (a/b/g).");
+									System.out.println("Apantise ksana");
+									answer = input.nextLine();
+				}
 				if (Normal.normalc4(arr[i].getPos() - 4).equals(answer)) {
 					return answ = true;
 				} else {
